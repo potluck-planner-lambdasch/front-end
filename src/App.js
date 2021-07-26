3ebfdfcf2e5e798528a8c4466dfeca94596e6622
Kyle Andrews <andrewskr90@gmail.com>
2021-07-26 11:00:11 -0700

    username/password inputs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+        <Link to='/login'>Log in</Link>
+
+        <Route path='/login'>
+        <div class='login'>
+          <form>
+            <label>Username
+              <input
+                type='text'
+                name='username'
+                id='username'
+              />
+            </label>
+            <label>Password
+              <input
+                type='password'
+                name='password'
+                id='password'
+              />
+            </label>
+          </form>
+        </div>
+        </Route>
+      
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload unit 4 build week.
