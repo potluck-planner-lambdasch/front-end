3812e3a601d2dd6ada1e5f9c4b6bdb924d790d95
Kyle Andrews <andrewskr90@gmail.com>
2021-07-26 13:41:35 -0700

    login form-schema set up

---
+++ src/validation/formSchema.js
@@ -0,0 +1,17 @@
+import * as yup from 'yup'
+
+const formSchema = yup.object().shape({
+    username: yup
+        .string()
+        .trim()
+        .required('username required')
+        .min(5, 'username must be at least 5 characters'),  
+    password: yup
+        .string()
+        .trim()
+        .required('password required')
+        .min(5, 'password must be at least 5 characters')
+        
+})
+
+export default formSchema
